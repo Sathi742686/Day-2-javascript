654e8efb80da2db1e8855717cf401e85ee6cd419
Sathi742686 <farjanaahmmed742686@gmail.com>
2022-12-17 01:46:07 +0600

    all javascript code completed

---
+++ script.js
@@ -0,0 +1,145 @@
+// let firstName = "Asif";
+// let age = 33;
+// console.log(firstName);
+// console.log(age);
+
+// 1. Object
+// 2. primitive
+
+/*
+ 1. Number
+ 2. String
+ 3. Boolean
+ 4. Undifind
+ 5. Null
+//  6. Symble
+//  7. Bigint
+*/
+
+// // 1. Number
+// let age = 28;
+// console.log(age);
+
+// // 2. Sting
+// let firstName = "Asif 10 ";
+// console.log(firstName);
+
+// // 3. Boolean
+// let isSinger = true;
+// console.log(isSinger);
+
+// // 4. Undifined
+// let homeCity;
+// console.log(homeCity);
+
+// // 5. Null
+// let income = null;
+// console.log(income);
+
+// console.log(age);
+
+// // console.log(typeof age);
+// // console.log(typeof homeCity);
+// // consol.log(typeof income);
+
+// console.log(homeCity);
+// homeCity = "Dhaka";
+// console.log(homeCity);
+
+// console.log(typeof isSinger);
+// isSinger = "Banladeshi Singer";
+// console.log(typeof isSinger);
+// console.log(typeof income); //javascript bug
+
+// let firstName = "Asif";
+
+/*
+1.let
+2.const
+3.var
+*/
+
+// // 1. let
+// let myage = 21;
+// console.log(myage);
+// myage = 23;
+// console.log(myage);
+
+// // 2. const
+// const birthYear = 2001;
+// console.log(birthYear);
+
+// // 3. var
+// var job = "Teacer";
+// console.log(job);
+
+// let profit = 32 - 28;
+// console.log(profit);
+
+// let currenYear = 2022;
+
+// let myAge = currenYear - 2001;
+// console.log(myAge);
+
+// let iyadAge = currenYear - 2020;
+// console.log(iyadAge);
+
+// console.log(myAge, iyadAge);
+
+// let ageDiff = myAge - iyadAge;
+// console.log(ageDiff);
+
+// +
+// _
+// *
+// /
+// **
+// %
+// ++
+// --
+
+// console.log(10 + 5);
+// console.log(25 - 9);
+// console.log(5 * 5);
+// console.log(30 / 5);
+// console.log(3 ** 3);
+// console.log(25 % 3);
+
+// let x = 10;
+// x++;
+// console.log(x);
+
+// let y = 20;
+// y--;
+// console.log(y);
+
+// let firstName = "Asif";
+// let lastName = "Akbar";
+
+// console.log(firstName + " " + lastName);
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+let item = 10 + 5;
+console.log(item);
+
+let x = 15;
+x += 10; // x = x + 10
+console.log(x);
+
+let y = 20;
+y -= 15; // y = y - 15
+console.log(y);
+
+let z = 5;
+z *= 5; // z = z * 5
+// z = 5 * 5
+console.log(z);
+
+let w = 15;
+w /= 3; // w = w / 15
+console.log(w);
